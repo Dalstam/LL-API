1fd7c828f4cfc3fcece834c24dde9edb94c58f89
cwtwn <91606640+cwtwn@users.noreply.github.com>
2021-10-27 15:26:05 +0200

    layouts

--- layouts/menu.vue
+++ layouts/menu.vue
@@ -1,7 +1,5 @@
 <template>
   <main>
     <NavBar></NavBar>
-    <Nuxt />
-    <Footer></Footer>
   </main>
 </template>
--- layouts/voorwaarden-app.vue
+++ layouts/voorwaarden-app.vue
@@ -4,30 +4,3 @@
   </main>
 </template>
 
-<script>
-export default {
-   head() {
-       return {
-           meta: [
-               {
-                  property: "og:title",
-                  content: "http://www.tringtring.nl/"
-               },
-                {
-                  property: "og:title",
-                  content: "Het nieuwe bakfiets bezorgnetwerk met liefde voor horeca in Amsterdam en Utrecht"
-               },
-                {
-                  property: "og:title",
-                  content: "Wij bevoorraden dagelijks meer dan 350 restaurants en hotels in Amsterdam en Utrecht. Alleen op de de bakfiets. On-demand. 100% groen. 7 dagen per week."
-               },
-                {
-                  name: "description",
-                  content: "TringTring is hét antwoord op de autovrije binnenstad. Dat doen we op de fiets: zo maken we de stad een beetje mooier."
-                } 
-           ]
-       }
-
-    }
-}
-</script>
