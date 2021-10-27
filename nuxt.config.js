e63af944f2e3e610310a2ad5929e8cc3553ef5ff
Dalstam <danielvdstam@gmail.com>
2021-10-27 14:26:49 +0200

    meta tags 1 commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,31 +5,31 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'TRINGTRING_ FIETSEN MET HORECA',
+    title: 'TringTring - Het nieuwe bakfiets bezorgnetwerk.',
     htmlAttrs: {
       lang: 'nl'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "TringTring is hét antwoord op de autovrije binnenstad. Dat doen we op de fiets: zo maken we de stad een beetje mooier." },
+      { hid: '000', name: 'description', content: "TringTring is hét antwoord op de autovrije binnenstad. Dat doen we op de fiets: zo maken we de stad een beetje mooier." },
       { name: 'format-detection', content: 'telephone=no' },
       { name: "twitter:card", content: "summary" },
       { name: "twitter:site", content: "@TringTring" },
-      { property: "og:title", content: "Het nieuwe bakfiets bezorgnetwerk met liefde voor horeca in Amsterdam en Utrecht" },
-      { property: "og:description", content: "Wij bevoorraden dagelijks meer dan 350 restaurants en hotels in Amsterdam en Utrecht. Alleen op de de bakfiets. On-demand. 100% groen. 7 dagen per week." },
+      { hid: '1', property: "og:title", content: "Het nieuwe bakfiets bezorgnetwerk met liefde voor horeca in Amsterdam en Utrecht" },
+      { hid: '2', property: "og:description", content: "Wij  dagelijks meer dan 350 restaurants en hotels in Amsterdam en Utrecht. Alleen op de de bakfiets. On-demand. 100% groen. 7 dagen per week." },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     script: [
-      {src:"scripts/jquery/jquery.js"},
-      {src:"scripts/bootstrap/js/transition.js"},
-      {src:"scripts/bootstrap/js/carousel.js"},
-      {src:"scripts/placeholders/placeholders.min.js"},
-      {src:"scripts/jquery-validation/dist/jquery.validate.min.js"},
-      {src:"scripts/jquery-validation/src/localization/messages_nl.js"}
-     ]
+      { src: "scripts/jquery/jquery.js" },
+      { src: "scripts/bootstrap/js/transition.js" },
+      { src: "scripts/bootstrap/js/carousel.js" },
+      { src: "scripts/placeholders/placeholders.min.js" },
+      { src: "scripts/jquery-validation/dist/jquery.validate.min.js" },
+      { src: "scripts/jquery-validation/src/localization/messages_nl.js" }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -52,7 +52,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-  
+
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
