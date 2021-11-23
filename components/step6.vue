02e4263b3e497daa55d704febeaf7174f2b15efb
coent <coenteeuwen@gmail.com>
2021-11-23 11:14:16 +0100

    Step 6 vuex data koppeling

--- components/step6.vue
+++ components/step6.vue
@@ -1,24 +1,28 @@
-<template>
-    <!-- STEP 6 -->
-					<div class="pm-step">
-						<h1 class="form-title">Waar zou je het liefst op fietsen?</h1>
-						<label class="form-group text-left input-lg form-control">
-							<input type="checkbox" name="Fiets" value="Racefiets" v-model="Fiets" @change="welkeFiets(value)">
-							<span>Racefiets</span>
-							<img class="vink" src="images/vink-blue.png" />
-						</label>
-						<label class="form-group text-left input-lg form-control">
-							<input type="checkbox" name="Fiets" value="Bakfiets" v-model="Fiets" @change="welkeFiets(value)">
-							<span>Elektrische bakfiets</span>
-							<img class="vink" src="images/vink-blue.png" />
-						</label>
-						<label class="form-group text-left input-lg form-control">
-							<input type="checkbox" name="Fiets" value="Goupil" v-model="Fiets" @change="welkeFiets(value)">
-							<span>Goupil (Picnic wagentje)</span>
-							<img class="vink" src="images/vink-blue.png" />
-						</label>
-					</div>
+<template>	
+	<div>
+		<!-- STEP 6 -->
+		<div class="pm-step">
+			<h1 class="form-title">Waar zou je het liefst op fietsen?</h1>
+			<label class="form-group text-left input-lg form-control">
+				<input type="radio" value="raceFiets" v-model="Fiets" @change="welkeFiets"> 
+				<span>Racefiets</span>
+				<img class="vink" src="images/vink-blue.png" />
+			</label>
+			<label class="form-group text-left input-lg form-control">
+				<input type="radio" value="bakFiets" v-model="Fiets" @change="welkeFiets">
+				<span>Elektrische bakfiets</span>
+				<img class="vink" src="images/vink-blue.png" />
+			</label>
+			<label class="form-group text-left input-lg form-control">
+				<input type="radio" value="goupil" v-model="Fiets" @change="welkeFiets">
+				<span>Goupil (Picnic wagentje)</span>
+				<img class="vink" src="images/vink-blue.png" />
+			</label>
+		</div>
+	</div>
 </template>
+
+
 <script>
 export default {
   data() {
@@ -27,7 +31,7 @@ export default {
     }
   },
   methods: {
-    welkeFiets(value) {
+    welkeFiets() {
       this.$store.commit('setwelkeFiets', {
         Fiets: this.Fiets
       })
